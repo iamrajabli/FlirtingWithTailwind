de933eb2d6ebd5534ea98606f99e7766bd0fca2c
iamrajabli <iamrajabli@outlook.com>
2022-07-10 00:03:27 +0400

    Project completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,15 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        '15' : '3.75rem'
+        '15': '3.75rem'
       },
       borderWidth: {
         '1': '1px',
       },
-      colors : {
-        'proto': '#32445a'
+      colors: {
+        'proto': '#32445a',
+        'prototurk': '#32445a',
+        'twitter': '#1da1f2'
       }
     },
   },
